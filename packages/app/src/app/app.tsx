bebeddd8bd8bcd3dbaf21b0c49099c0901c6cdec
angelcode1 <wildchild.313@gmail.com>
2026-08-29 23:56:46 +1000

    Render production preview alongside Ribbon

--- packages/app/src/app/app.tsx
+++ packages/app/src/app/app.tsx
@@ -1,3 +1,11 @@
+import './app.controller';
+
+import { Preview } from '../features/preview';
 import { Ribbon } from '../features/ribbon';
 
-export const App = (): JSX.Element => <Ribbon />;
+export const App = (): JSX.Element => (
+	<>
+		<Ribbon />
+		<Preview />
+	</>
+);
