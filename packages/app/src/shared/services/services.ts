bf5aa2c3b267b7ea3bfe2585b4821744276389b5
angelcode1 <wildchild.313@gmail.com>
2026-08-29 23:55:29 +1000

    Wire activation and surface services

--- packages/app/src/shared/services/services.ts
+++ packages/app/src/shared/services/services.ts
@@ -1,14 +1,18 @@
+import { ActivationService } from './activation';
 import { LauncherService } from './launcher/model/launcher.service';
 import { AppManagerProvider } from './launcher/providers/app-manager/app-manager.provider';
 import { InputProvider } from './launcher/providers/input-manager/input-manager.provider';
 import { InternalProvider } from './launcher/providers/internal/internal.provider';
 import { LifecycleManagerService } from './lifecycle-manager';
 import { SettingsService } from './settings';
+import { SurfaceService } from './surface';
 import { SystemInfoService } from './system-info';
 
+export const activationService = new ActivationService();
 export const systemInfoService = new SystemInfoService();
 export const settingsService = new SettingsService();
-export const lifecycleManagerService = new LifecycleManagerService(systemInfoService);
+export const surfaceService = new SurfaceService(systemInfoService);
+export const lifecycleManagerService = new LifecycleManagerService();
 
 export const launchPointProviders = [
 	new InputProvider(),
@@ -18,6 +22,6 @@ export const launchPointProviders = [
 
 export const launcherService = new LauncherService(
 	settingsService,
-	lifecycleManagerService,
+	surfaceService,
 	launchPointProviders,
 );
