15a19b1b31bf0c3039033694f50a86bf4f88b3de
angelcode1 <wildchild.313@gmail.com>
2026-08-29 23:52:23 +1000

    Narrow lifecycle manager to foreign app events

--- packages/app/src/shared/services/lifecycle-manager/api/lifecycle-manager.interface.ts
+++ packages/app/src/shared/services/lifecycle-manager/api/lifecycle-manager.interface.ts
@@ -1,10 +1,3 @@
 export type LifecycleManagerEvents = {
-	relaunch: void;
-	requestHide: void;
-};
-
-export type VisibilityController = {
-	isVisible: () => boolean;
-	requestHide: () => void;
-	waitUntilHidden: () => Promise<void>;
+	foreignLaunch: void;
 };
