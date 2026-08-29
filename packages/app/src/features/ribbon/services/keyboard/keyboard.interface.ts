c9f469715edcab0b3e456d980c5ecb031cb1c26f
angelcode1 <wildchild.313@gmail.com>
2026-08-29 23:55:49 +1000

    Add preview keyboard owner

--- packages/app/src/features/ribbon/services/keyboard/keyboard.interface.ts
+++ packages/app/src/features/ribbon/services/keyboard/keyboard.interface.ts
@@ -1,5 +1,5 @@
 export type Shift = -1 | 1;
-export type KeyboardOwner = 'ribbon' | 'drawer' | 'keypad';
+export type KeyboardOwner = 'ribbon' | 'drawer' | 'keypad' | 'preview';
 export type RemoteDigit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
 
 export type KeyboardOwnerHandlers = {
