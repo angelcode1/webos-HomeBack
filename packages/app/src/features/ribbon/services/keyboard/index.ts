f8120d3701fc7e14d27eda8115e156f623d2c6dc
angelcode1 <wildchild.313@gmail.com>
2026-08-30 00:06:00 +1000

    Export keyboard ownership types

--- packages/app/src/features/ribbon/services/keyboard/index.ts
+++ packages/app/src/features/ribbon/services/keyboard/index.ts
@@ -1,2 +1,8 @@
 export { KeyboardService } from './keyboard.service';
 export { HOLD_THRESHOLD_MS } from './keyboard.lib';
+export type {
+	KeyboardOwner,
+	KeyboardOwnerHandlers,
+	RemoteDigit,
+	Shift,
+} from './keyboard.interface';
