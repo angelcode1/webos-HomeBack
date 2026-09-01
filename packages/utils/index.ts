86259072d08b89cd82e8c34f68a6be0a200a771a
angelcode1 <wildchild.313@gmail.com>
2026-09-01 19:30:06 +1000

    Export shared utility helpers

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,1 +1,11 @@
 export { AsyncSink } from './async-sink';
+export {
+	COLOUR_MICOM_KEYCODES,
+	colourMicomKeycode,
+	micomKeycodeForUinput,
+	NUMERIC_MICOM_KEYCODES,
+	numericMicomKeycode,
+	UINPUT_TO_MICOM,
+	type MicomColour,
+} from './micom-keycodes';
+export { clampInteger, isPlainObject, normalizedText } from './value';
