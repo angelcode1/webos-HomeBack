24842eef679c1be2ca34642d2493db8437cc7d47
angelcode1 <wildchild.313@gmail.com>
2026-09-01 20:34:59 +1000

    Use explicit TypeScript exports for native tests

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,9 +1,9 @@
-export { AsyncSink } from './async-sink';
+export { AsyncSink } from './async-sink.ts';
 export {
 	APPLICATION_MANAGER_URI,
 	HOME_BACK_PREVIEW_INTENT,
 	HOME_BACK_SHOW_INTENT,
-} from './constants';
+} from './constants.ts';
 export {
 	COLOUR_MICOM_KEYCODES,
 	colourMicomKeycode,
@@ -12,5 +12,5 @@ export {
 	numericMicomKeycode,
 	UINPUT_TO_MICOM,
 	type MicomColour,
-} from './micom-keycodes';
-export { clampInteger, isPlainObject, normalizedText } from './value';
+} from './micom-keycodes.ts';
+export { clampInteger, isPlainObject, normalizedText } from './value.ts';
