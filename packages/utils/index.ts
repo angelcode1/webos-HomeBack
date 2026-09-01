55c726bcd5298cbb09dfdbee7c16e0536d6a5f12
angelcode1 <wildchild.313@gmail.com>
2026-09-01 19:41:05 +1000

    Export shared protocol constants

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,5 +1,10 @@
 export { AsyncSink } from './async-sink';
 export {
+	APPLICATION_MANAGER_URI,
+	HOME_BACK_PREVIEW_INTENT,
+	HOME_BACK_SHOW_INTENT,
+} from './constants';
+export {
 	COLOUR_MICOM_KEYCODES,
 	colourMicomKeycode,
 	micomKeycodeForUinput,
