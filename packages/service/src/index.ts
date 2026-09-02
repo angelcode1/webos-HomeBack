6d0afce503faa090c06114aec43c6d90922a79d4
angelcode1 <wildchild.313@gmail.com>
2026-09-03 07:08:53 +1000

    Expose cached current weather

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -10,9 +10,13 @@ import {
 } from './notification';
 import { PreviewNotificationService } from './preview-notification-service';
 import { getUid } from './utils';
+import { WeatherService } from './weather';
 
 const NOTIFICATION_URI = 'luna://com.webos.notification';
 const service = new Service();
+const weatherService = new WeatherService((uri, params, timeoutMs) =>
+	service.oneshot(uri, params, timeoutMs),
+);
 const previewNotificationState = new PreviewNotificationState();
 const previewNotificationService = new PreviewNotificationService(
 	previewNotificationState,
@@ -89,6 +93,11 @@ service.registerSimple('/remote/start', async () => {
 service.registerSimple('/remote/status', () => ({
 	done: true,
 	status: serviceStatus(),
+}));
+
+service.registerSimple('/weather/current', async () => ({
+	done: true,
+	weather: await weatherService.current(),
 }));
 
 service.registerSimple<PreviewNotificationRequest>('/notification/createPreviewToast', request =>
