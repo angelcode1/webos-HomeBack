84a27982ec544bd30f0371c0810381fab5751ec5
angelcode1 <wildchild.313@gmail.com>
2026-09-10 10:44:18 +1000

    build: emit normal HomeBack and camera PiP companion IPKs

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,28 @@
 import { hoc } from '@webosbrew/webos-packager-plugin';
 
 import app from '@homeback/app/webpack.config';
+import pipApp from '@homeback/pip-app/webpack.config';
 import service from '@homeback/service/webpack.config';
 
+import { PIP_APP_ID } from './build/project';
 import { id, version } from './package.json';
 
-export default hoc({
+const homeBackPackage = hoc({
 	id,
 	version,
 	app,
 	services: [service],
 });
+
+const cameraPipPackage = hoc({
+	id: PIP_APP_ID,
+	version,
+	app: pipApp,
+});
+
+const config = (
+	env: { WEBPACK_SERVE?: boolean },
+	argv: { mode?: 'none' | 'development' | 'production' },
+) => [...homeBackPackage(env, argv), ...cameraPipPackage(env, argv)];
+
+export default config;
