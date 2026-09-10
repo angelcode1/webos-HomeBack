e744f719a41cad860191f411cc84956d0d919b0e
angelcode1 <wildchild.313@gmail.com>
2026-09-10 10:41:34 +1000

    build: define dedicated PiP companion app id

--- build/project.ts
+++ build/project.ts
@@ -3,3 +3,4 @@ import { id, version } from '../package.json';
 export const APP_ID = id;
 export const APP_VERSION = version;
 export const SERVICE_ID = `${APP_ID}.service`;
+export const PIP_APP_ID = `${APP_ID}.camera`;
